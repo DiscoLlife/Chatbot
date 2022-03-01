8d3207f14a3c768f0e3a97d9546179acc96c09ff
DavisKinloch <79967193+DiscoLlife@users.noreply.github.com>
2022-03-01 23:24:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Start with Greeting
+
+#User inputs reply to greeting
+
+#taking key nouns out of text to decipher see: https://spacy.io/ to decifer text patterns
+
+#list of patterns 
+
+#Reads the Character Text File
+text_file = open('CharacterText.txt')
+text = text_file.read()
+print(text)
