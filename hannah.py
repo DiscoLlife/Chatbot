d1a72f0987bd6c0b15df53a1c14dc461924b2197
Davis Kinloch <79967193+DiscoLlife@users.noreply.github.com>
2022-03-04 01:05:51 +0000

    Detects verbs entitys and nouns within the given character text

---
+++ hannah.py
@@ -0,0 +1,1 @@
+#Hannah's GUI project for the chatbot
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#import hannah
+
 #Start with Greeting
 
 #User inputs reply to greeting
@@ -7,6 +9,28 @@
 #list of patterns 
 
 #Reads the Character Text File
+import spacy
+textnouns = []
+textentity = []
+textverb = []
+#Opens the .txt file and puts it throught natural language processing (nlp)
+nlp = spacy.load('en_core_web_sm')
 text_file = open('CharacterText.txt')
 text = text_file.read()
-print(text)
+text = text.replace('\n', ' ')
+text = nlp(text)
+#seperates nouns into a list
+textverb = [token.lemma_ for token in text if token.pos_ == "VERB"]
+textnouns = [chunk.text for chunk in text.noun_chunks]
+
+#seperates entities such as numbers or people into a seperate list and labels them
+position = 0
+for entity in text.ents:
+  textentity.append([entity.text])
+  textentity[position].append(entity.label_)
+  position += 1
+
+#prints the nouns and entities
+print(textnouns)
+print(textverb)
+print(textentity)
